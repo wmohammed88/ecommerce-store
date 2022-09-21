9654b5c7122120764b2511b7673b77c218303bb9
wmohammed88 <wmohammed88@gmail.com>
2022-09-20 23:40:44 -0500

    Final commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,19 @@
-import '../styles/globals.css'
+import React from 'react';
+import { Toaster } from 'react-hot-toast';
 
-import {Layout} from '../components';
+import { Layout } from '../components';
+import '../styles/globals.css';
+import { StateContext } from '../context/StateContext';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
   )
-  
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,23 @@
-import React from 'react'
-import product from '../first ecommerce website/schemas/product';
+import React from 'react';
+
 import { client } from '../lib/client';
 import { Product, FooterBanner, HeroBanner } from '../components';
-import banner from '../first ecommerce website/schemas/banner';
 
-const Home = ({products, bannerData}) => {
-  return (
-   <div>
-    <HeroBanner heroBanner={bannerData.length && bannerData[0]}/>
-    <div className='products-heading'>
-      <h2>Best selling products</h2>
-      <p>Best headphones on the market</p>
-    </div>
-    <div className='products-container'>
-      {products?.map((product) => <Product key={product._id} product={product}/>)}
+const Home = ({ products, bannerData }) => (
+  <div>
+    <HeroBanner heroBanner={bannerData.length && bannerData[0]}  />
+    <div className="products-heading">
+      <h2>Best Seller Products</h2>
+      <p>speaker There are many variations passages</p>
     </div>
 
-    <FooterBanner footerBanner={bannerData && bannerData[0]}/>
-    </div> 
-  )
-}
+    <div className="products-container">
+      {products?.map((product) => <Product key={product._id} product={product} />)}
+    </div>
+
+    <FooterBanner footerBanner={bannerData && bannerData[0]} />
+  </div>
+);
 
 export const getServerSideProps = async () => {
   const query = '*[_type == "product"]';
@@ -29,7 +27,7 @@ export const getServerSideProps = async () => {
   const bannerData = await client.fetch(bannerQuery);
 
   return {
-    props: {products, bannerData}
+    props: { products, bannerData }
   }
 }
 
